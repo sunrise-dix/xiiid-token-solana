821132b02c7c8138dac00a7f5ef147eae821bb0b
sunrise-dix <sunrise@dixsoft.kr>
2026-02-04 13:26:01 -0800

    [update] change declare id

--- programs/xiiid-token-solana/src/lib.rs
+++ programs/xiiid-token-solana/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
-declare_id!("6FuCSdKgB9uZKvSY8wEMEKs72fRyqsyhkYAL4eJjRTT6");
+declare_id!("CVrU5e1iwZ2PB9fVJGyCFSWoPCALNMz6fBhZFFMouNq2");
 
 // --- Constants & Space Calculation ---
 const MAX_CLASS_ID: usize = 64;
