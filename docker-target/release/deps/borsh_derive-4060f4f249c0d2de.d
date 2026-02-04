3854a0ddf97cd061be74f65626f41a25ec1dc4a2
sunrise-dix <sunrise@dixsoft.kr>
2026-02-04 13:28:06 -0800

    [deploy]

---
+++ docker-target/release/deps/borsh_derive-4060f4f249c0d2de.d
@@ -0,0 +1,5 @@
+/workdir/docker-target/release/deps/libborsh_derive-4060f4f249c0d2de.so: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/borsh-derive-0.10.4/src/lib.rs
+
+/workdir/docker-target/release/deps/borsh_derive-4060f4f249c0d2de.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/borsh-derive-0.10.4/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/borsh-derive-0.10.4/src/lib.rs:
---
+++ docker-target/sbpf-solana-solana/release/deps/solana_borsh-3923639b93f814e7.d
@@ -0,0 +1,11 @@
+/workdir/docker-target/sbpf-solana-solana/release/deps/libsolana_borsh-3923639b93f814e7.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/deprecated.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/macros.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v0_10.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v1.rs
+
+/workdir/docker-target/sbpf-solana-solana/release/deps/libsolana_borsh-3923639b93f814e7.rlib: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/deprecated.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/macros.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v0_10.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v1.rs
+
+/workdir/docker-target/sbpf-solana-solana/release/deps/solana_borsh-3923639b93f814e7.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/deprecated.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/macros.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v0_10.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v1.rs
+
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/deprecated.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/macros.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v0_10.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/solana-borsh-2.2.1/src/v1.rs:
